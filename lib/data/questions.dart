0b29137e564db39a6976e9861d133e62c6951b49
Vincent-zhangS <vzhang2022@gmail.com>
2026-02-12 11:40:15 -0500

    made quiz questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,58 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What is Waldo best known for?',
+    [
+      'Hiding in crowds',
+      'Flying planes',
+      'Cooking food',
+      'Solving crimes',
+    ],
+  ),
+  QuizQuestion(
+    'What colors does Waldo usually wear?',
+    [
+      'Red and white',
+      'Blue and yellow',
+      'Green and black',
+      'Purple and orange',
+    ],
+  ),
+  QuizQuestion(
+    'What item does Waldo often carry?',
+    [
+      'A walking stick',
+      'A sword',
+      'A camera',
+      'A backpack',
+    ],
+  ),
+  QuizQuestion(
+    'Where does Waldo usually appear?',
+    [
+      'In crowded scenes',
+      'In space',
+      'Underwater',
+      'On a farm',
+    ],
+  ),
+  QuizQuestion(
+    'What makes Waldo hard to find?',
+    [
+      'He blends into busy backgrounds',
+      'He hides in plain sight',
+      'He runs very clever',
+      'Scooby-Doo',
+    ],
+  ),
+  QuizQuestion(
+    'SCP-4885 also known as',
+    [
+      'The reader',
+      'Waldo',
+      'Find Him',
+      'Online Friend',
+    ],
+  ),
+];
