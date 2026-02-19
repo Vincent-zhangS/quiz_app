b3af76895a1250a1f14fc07f917c1ebbca743ed2
Vincent-zhangS <vzhang2022@gmail.com>
2026-02-19 11:55:39 -0500

    added logic

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,21 +14,26 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'THIS IS THE QUESTION',
+          Text(
+            currentQuestion.question,
+
             style: TextStyle(color: Colors.white, fontSize: 28),
           ),
           const SizedBox(height: 30),
-
-          ElevatedButton(onPressed: () {}, child: const Text("Answer 1")),
-          ElevatedButton(onPressed: () {}, child: const Text("Answer 2")),
-          ElevatedButton(onPressed: () {}, child: const Text("Answer 3")),
-          ElevatedButton(onPressed: () {}, child: const Text("Answer 4")),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(
+              answerText: item,
+              onTap: () {
+                // Handle answer selection
+              },
+            );
+          }),
         ],
       ),
     );
