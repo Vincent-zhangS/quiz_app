6433dc68834d5dfb9b2c724748e36a998b9c6c24
Vincent-zhangS <vzhang2022@gmail.com>
2026-02-19 16:50:09 -0500

    Added questions class along with different questions and answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -5,40 +5,43 @@ import 'package:quiz_app/data/questions.dart';
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
-  @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
+
     return SizedBox(
       width: double.infinity,
       child: Container(
-        margin: const EdgeInsets.all(30),
-        child: Column(
+        margin: const EdgeInsets.all(25),
+        child: Column (
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
-              currentQuestion.question,
-
-              style: TextStyle(color: Colors.white, fontSize: 28),
-              textAlign: TextAlign.center,
+            Text(currentQuestion.question,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28
             ),
+            textAlign: TextAlign.center,
+          ),
             const SizedBox(height: 30),
-            ...currentQuestion.answers.map((item) {
-              return AnswerButton(
-                answerText: item,
-                onTap: () {
-                },
-              );
+            ...currentQuestion.getShuffledAnswers().map((item) {
+              return AnswerButton(answerText: item,
+              onTap: answerQuestion);
             }),
-          ],
-        ),
+        ],),
       ),
     );
   }
