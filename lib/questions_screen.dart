b2c5a82da8d6bcef7fa248cfcbbc432a4457b54d
Vincent-zhangS <vzhang2022@gmail.com>
2026-03-03 12:13:59 -0500

    installed google font

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
+  @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
@@ -17,31 +19,35 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       currentQuestionIndex++;
     });
   }
-  @override
+
   Widget build(BuildContext context) {
     final currentQuestion = questions[currentQuestionIndex];
-
     return SizedBox(
       width: double.infinity,
       child: Container(
-        margin: const EdgeInsets.all(25),
-        child: Column (
+        margin: const EdgeInsets.all(30),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(currentQuestion.question,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28
+            Text(
+              currentQuestion.question,
+
+              style: GoogleFonts.roboto(color: Colors.white, fontSize: 28),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((item) {
-              return AnswerButton(answerText: item,
-              onTap: answerQuestion);
+              return AnswerButton(
+                answerText: item,
+                onTap: () {
+                  answerQuestion();
+                  // Handle answer selection
+                },
+              );
             }),
-        ],),
+          ],
+        ),
       ),
     );
   }
